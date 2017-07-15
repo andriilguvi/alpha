f95fddd3907ea60305855c78fb50cb9dbbf35345
andriilguvi <andriilguvi68@gmail.com>
2017-07-15 10:17:03 +0530

    Create alpha.java

---
+++ alpha.java
@@ -0,0 +1,17 @@
+import java.io.*
+import java.util.*;
+class alpha
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+for(char i='a';i<='z';i++)
+{
+for(char j='A';j<='Z';j++)
+{
+System.out.print("alphabet");
+}
+}
+}
+}
+
